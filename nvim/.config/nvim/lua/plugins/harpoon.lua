a0b69d7894f76a589a0f19d08b832838406fba79
Nataneljpwd <natanelrudyuk@gmail.com>
2026-05-22 17:11:12 +0000

    fixed the configuration, start on work for harpoon

---
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,14 @@
+local add = require("packages")
+
+add(
+	{
+		"nvim-lua/plenary.nvim",
+		{
+			"ThePrimeagen/harpoon",
+			version = "harpoon2",
+		}
+	}
+)
+
+local harpoon = require("harpoon")
+-- TODO: finish
